63c11bbfd3b5be65c49bac7cc6a57ff1f6049a98
ivan-drr <ivan.delrr@gmail.com>
2020-08-20 21:34:40 +0200

    sidenav prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,25 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashScreenComponent
+    SplashScreenComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
